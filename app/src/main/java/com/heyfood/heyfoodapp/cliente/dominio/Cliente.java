b5b2d3582a916921a49402049f999e5982634cfd
gabriel.caboclo <gabriel.cabocloe14@gmail.com>
2019-05-14 13:20:57 -0300

    Getters e Setters.

--- app/src/main/java/com/heyfood/heyfoodapp/cliente/dominio/Cliente.java
+++ app/src/main/java/com/heyfood/heyfoodapp/cliente/dominio/Cliente.java
@@ -8,4 +8,12 @@ import java.util.Date;
 
 public class Cliente {
     private Date dataCadastro;
+
+    public Date getDataCadastro() {
+        return dataCadastro;
+    }
+
+    public void setDataCadastro(Date dataCadastro) {
+        this.dataCadastro = dataCadastro;
+    }
 }
--- app/src/main/java/com/heyfood/heyfoodapp/pessoa/dominio/Pessoa.java
+++ app/src/main/java/com/heyfood/heyfoodapp/pessoa/dominio/Pessoa.java
@@ -10,4 +10,28 @@ public class Pessoa {
     private String nome;
     private String cpf;
     private Date dataNAscimento;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public Date getDataNAscimento() {
+        return dataNAscimento;
+    }
+
+    public void setDataNAscimento(Date dataNAscimento) {
+        this.dataNAscimento = dataNAscimento;
+    }
 }
