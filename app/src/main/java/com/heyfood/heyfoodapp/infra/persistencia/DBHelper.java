f2b5e6c0723f5f6753609669cd756f61c069c9b2
jonathasfsilva <jonathas_ssilva@hotmail.com>
2019-05-15 09:54:11 -0300

    metodos criar tabelas drop tables

--- app/src/main/java/com/heyfood/heyfoodapp/infra/persistencia/DBHelper.java
+++ app/src/main/java/com/heyfood/heyfoodapp/infra/persistencia/DBHelper.java
@@ -23,4 +23,29 @@ public class DBHelper extends SQLiteOpenHelper {
         super(HeyFoodApp.getContext(), NOME_DB,null, VERSAO);
     }
 
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        createTabelaUsuario(db);
+    }
+
+    private void createTabelaUsuario(SQLiteDatabase db) {
+        String sqlTbUsuario =
+                "CREATE TABLE %1$s ( "  +
+                        "  %2$s INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                        "  %3$s TEXT NOT NULL UNIQUE, " +
+                        "  %4$s TEXT NOT NULL " +
+                        ");";
+        sqlTbUsuario = String.format(sqlTbUsuario,
+                TABELA_USUARIO, CAMPO_ID, CAMPO_LOGIN, CAMPO_PASSWORD);
+        db.execSQL(sqlTbUsuario);
+    }
+    private void dropTables(SQLiteDatabase db) {
+        StringBuilder dropTables = new StringBuilder();
+        for (String tabela : TABELAS) {
+            dropTables.append(" DROP TABLE IF EXISTS ");
+            dropTables.append(tabela);
+            dropTables.append("; ");
+        }
+        db.execSQL(dropTables.toString());
+    }
 }
