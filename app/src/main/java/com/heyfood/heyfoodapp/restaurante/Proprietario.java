4ad51950c82314ee2b4c352eb28b572cdc9ba606
jonathasfsilva <jonathas_ssilva@hotmail.com>
2019-05-23 09:44:02 -0300

    cria classe proprietario

---
+++ app/src/main/java/com/heyfood/heyfoodapp/restaurante/Proprietario.java
@@ -0,0 +1,29 @@
+package com.heyfood.heyfoodapp.restaurante;
+
+import com.heyfood.heyfoodapp.usuario.dominio.Usuario;
+
+public class Proprietario {
+    private Restaurante restaurante;
+    private Usuario usuario;
+
+    public Proprietario(Restaurante restaurante, Usuario usuario) {
+        this.restaurante = restaurante;
+        this.usuario = usuario;
+    }
+
+    public Restaurante getRestaurante() {
+        return restaurante;
+    }
+
+    public Usuario getUsuario() {
+        return usuario;
+    }
+
+    public void setRestaurante(Restaurante restaurante) {
+        this.restaurante = restaurante;
+    }
+
+    public void setUsuario(Usuario usuario) {
+        this.usuario = usuario;
+    }
+}
