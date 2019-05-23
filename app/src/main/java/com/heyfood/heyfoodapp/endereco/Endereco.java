2e487345faa8b81818f4f560ec48eb673c57659a
Bruno Santos <brunosantos@protonmail.com>
2019-05-23 09:08:43 -0300

    cria pacote e classe de endereço

---
+++ app/src/main/java/com/heyfood/heyfoodapp/endereco/Endereco.java
@@ -0,0 +1,69 @@
+package com.heyfood.heyfoodapp.endereco;
+
+public class Endereco {
+
+    private String rua;
+    private String bairro;
+    private String cidade;
+    private String cep;
+    private String numero;
+
+    public Endereco(String rua, String bairro, String cidade, String cep, String numero) {
+        this.rua = rua;
+        this.bairro = bairro;
+        this.cidade = cidade;
+        this.cep = cep;
+        this.numero = numero;
+    }
+
+    public String getRua() {
+        return rua;
+    }
+
+    public String getBairro() {
+        return bairro;
+    }
+
+    public String getCidade() {
+        return cidade;
+    }
+
+    public String getCep() {
+        return cep;
+    }
+
+    public String getNumero() {
+        return numero;
+    }
+
+    public void setRua(String rua) {
+        this.rua = rua;
+    }
+
+    public void setBairro(String bairro) {
+        this.bairro = bairro;
+    }
+
+    public void setCidade(String cidade) {
+        this.cidade = cidade;
+    }
+
+    public void setCep(String cep) {
+        this.cep = cep;
+    }
+
+    public void setNumero(String numero) {
+        this.numero = numero;
+    }
+
+    @Override
+    public String toString() {
+        return "Endereco{" +
+                "rua='" + rua + '\'' +
+                ", bairro='" + bairro + '\'' +
+                ", cidade='" + cidade + '\'' +
+                ", cep='" + cep + '\'' +
+                ", numero='" + numero + '\'' +
+                '}';
+    }
+}
