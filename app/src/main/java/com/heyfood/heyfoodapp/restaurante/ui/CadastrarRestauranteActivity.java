b0d207c54958fe8f946878d0f5e0016dc3b807ca
andre.csassis <andrecarlos459@gmail.com>
2019-05-28 14:41:49 -0300

    Tela Cadastrar Restaurante,Especialidade e Informações

---
+++ app/src/main/java/com/heyfood/heyfoodapp/restaurante/ui/CadastrarRestauranteActivity.java
@@ -0,0 +1,13 @@
+package com.heyfood.heyfoodapp.restaurante.ui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CadastrarRestauranteActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cadastrar_restaurante);
+    }
+}
---
+++ app/src/main/java/com/heyfood/heyfoodapp/restaurante/ui/EspecialidadeRestauranteActivity.java
@@ -0,0 +1,15 @@
+package com.heyfood.heyfoodapp.restaurante.ui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.heyfood.heyfoodapp.R;
+
+public class EspecialidadeRestauranteActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_especialidade_restaurante);
+    }
+}
