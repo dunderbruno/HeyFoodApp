6ba187914c0b0f494a826d6357dd4b5fb3887d51
andrecarlos26 <andrecarlos459@gmail.com>
2019-06-04 09:21:19 -0300

    Tela de preferências do cliente.

---
+++ app/src/main/java/com/heyfood/heyfoodapp/cliente/ui/PreferenciaClienteActivity.java
@@ -0,0 +1,13 @@
+package com.heyfood.heyfoodapp.cliente.ui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PreferenciaClienteActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_preferencia_cliente);
+    }
+}
