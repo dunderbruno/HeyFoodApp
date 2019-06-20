e637d50e0b3348a5790082dbfe05761603dcb53c
Bruno Santos <brunosantos@protonmail.com>
2019-06-20 15:51:13 -0300

    Classe Prato

---
+++ app/src/main/java/com/heyfood/heyfoodapp/prato/Prato.java
@@ -0,0 +1,23 @@
+package com.heyfood.heyfoodapp.prato;
+
+public class Prato {
+    private long id;
+    private String nome;
+    //TODO: FOTO
+
+    public long getId() {
+        return id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+}
