c912c2030815f4c03cd07609aeb5feb63e7863f5
Bruno Santos <brunosantos@protonmail.com>
2019-06-26 07:36:19 -0300

    atualiza atributos de Prato

--- app/src/main/java/com/heyfood/heyfoodapp/prato/dominio/Prato.java
+++ app/src/main/java/com/heyfood/heyfoodapp/prato/dominio/Prato.java
@@ -1,8 +1,12 @@
 package com.heyfood.heyfoodapp.prato.dominio;
 
+import java.math.BigDecimal;
+
 public class Prato {
     private long id;
     private String nome;
+    private String descricao;
+    private BigDecimal preco;
     //TODO: FOTO
 
     public long getId() {
@@ -13,6 +17,14 @@ public class Prato {
         return nome;
     }
 
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public BigDecimal getPreco() {
+        return preco;
+    }
+
     public void setId(long id) {
         this.id = id;
     }
@@ -20,4 +32,13 @@ public class Prato {
     public void setNome(String nome) {
         this.nome = nome;
     }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public void setPreco(BigDecimal preco) {
+        this.preco = preco;
+    }
 }
+
