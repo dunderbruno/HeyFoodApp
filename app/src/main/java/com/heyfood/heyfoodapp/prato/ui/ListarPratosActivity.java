88e3a96672eb4e9f7dee9b42b49977ae26f5e797
gabriel.caboclo <gabriel.cabocloe14@gmail.com>
2019-06-27 15:16:26 -0300

    inicio listar pratos

---
+++ app/src/main/java/com/heyfood/heyfoodapp/prato/ui/ListarPratosActivity.java
@@ -0,0 +1,15 @@
+package com.heyfood.heyfoodapp.prato.ui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.heyfood.heyfoodapp.R;
+
+public class ListarPratosActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_listar_pratos);
+    }
+}
